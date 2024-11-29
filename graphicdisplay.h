733d21b4072481de4577bf673325e5ed4ecfc0c6
Aarav Surana <surana.aarav20@gmail.com>
2024-11-29 10:01:13 -0500

    Typos

--- graphicdisplay.h
+++ graphicdisplay.h
@@ -4,48 +4,54 @@
 #include "xwindow.h"
 #include "game.h"
 #include <vector>
-#include <string>
+#include <unordered_map>
 
 class GraphicDisplay {
-    const int WINDOW_WIDTH = 800;
-    const int WINDOW_HEIGHT = 1000;
-    const int CELL_SIZE = 30;
-    const int BOARD_WIDTH = CELL_SIZE * 11;
-    const int BOARD_HEIGHT = CELL_SIZE * 18;
-    const int BOARD_OFFSET_X = 50;
-    const int BOARD1_OFFSET_Y = 150;
-    const int BOARD2_OFFSET_Y = 150;
-    const int TEXT_MARGIN = 10;
-
-    const int EMPTY = XWindow::Color::Black;
-    const int I_BLOCK = XWindow::Color::Cyan;
-    const int J_BLOCK = XWindow::Color::Blue;
-    const int L_BLOCK = XWindow::Color::Orange;
-    const int O_BLOCK = XWindow::Color::Yellow;
-    const int S_BLOCK = XWindow::Color::Green;
-    const int Z_BLOCK = XWindow::Color::Red;
-    const int T_BLOCK = XWindow::Color::Magenta;
-    const int SP_BLOCK = XWindow::Color::White;
-
-    Game* game;
+private:
     XWindow* window;
+    Game* game;
 
+    // Screen layout constants
+    static constexpr int WINDOW_WIDTH = 800;
+    static constexpr int WINDOW_HEIGHT = 1000;
+    static constexpr int BOARD_WIDTH = 330;
+    static constexpr int BOARD_HEIGHT = 540;
+    static constexpr int CELL_SIZE = 30;
+    static constexpr int BOARD_OFFSET_X = 50;
+    static constexpr int BOARD1_OFFSET_Y = 100;
+    static constexpr int BOARD2_OFFSET_Y = 100;
+    static constexpr int TEXT_MARGIN = 20;
+
+    // Color mapping
+    enum BlockColors {
+        EMPTY = XWindow::Color::White,
+        I_BLOCK = XWindow::Color::Cyan,
+        J_BLOCK = XWindow::Color::Blue,
+        L_BLOCK = XWindow::Color::Orange,
+        O_BLOCK = XWindow::Color::Yellow,
+        S_BLOCK = XWindow::Color::Green,
+        Z_BLOCK = XWindow::Color::Red,
+        T_BLOCK = XWindow::Color::Magenta,
+        SP_BLOCK = XWindow::Color::Brown
+    };
+
+    // Track previous state to minimize redrawing
     std::vector<std::vector<int>> previousPlayer1Board;
     std::vector<std::vector<int>> previousPlayer2Board;
+    int previousPlayer1Level = -1;
+    int previousPlayer1Score = -1;
+    int previousPlayer1HighScore = -1;
+    int previousPlayer2Level = -1;
+    int previousPlayer2Score = -1;
+    int previousPlayer2HighScore = -1;
 
-    int previousPlayer1Level;
-    int previousPlayer1Score;
-    int previousPlayer1HighScore;
-    int previousPlayer2Level;
-    int previousPlayer2Score;
-    int previousPlayer2HighScore;
-
-    void drawStaticGrid();
-    void drawGridLines(int offsetX, int offsetY);
+    // Helper methods
     int getColorForBlockType(Type blockType);
     void drawBoard(Player* player, int offsetX, int offsetY, std::vector<std::vector<int>>& previousBoard);
     void drawTextInfo(Player* player1, Player* player2);
     void drawNextBlock(Player* player, int offsetX, int offsetY);
+    void drawBoardWithGrid(Player* player, int offsetX, int offsetY, std::vector<std::vector<int>>& previousBoard);
+    void drawNextBlockSection(Player* player, int offsetX, int offsetY);
 
 public:
     GraphicDisplay(Game* game);
@@ -54,4 +60,4 @@ public:
     void update();
 };
 
-#endif
+#endif // GRAPHICDISPLAY_H
